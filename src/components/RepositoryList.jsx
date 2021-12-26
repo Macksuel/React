c2252e92c3e49309cfa8f7d3e4c74992a47e7853
Macksuel Carvalho <macksuel7@gmail.com>
2021-12-26 19:01:20 -0300

    Listagem

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,4 +1,6 @@
+import { useState, useEffect } from 'react';
 import { RepositoryItem } from "./RepositoryItem";
+import '../styles/repositories.scss';
 
 const repository = {
     name: 'Macksuel',
@@ -7,11 +9,24 @@ const repository = {
 }
 
 export function RepositoryList(){
+
+    const [repositories, setRepositories] = useState([]);
+
+    useEffect(()  => {
+        fetch('https://api.github.com/orgs/rocketseat/repos')
+        .then(response => response.json())
+        .then(data => setRepositories(data))
+    }, []) 
+
     return(
         <section className="repository-list">
             <h1>Lista de Repositórios</h1>
 
             <ul>
+                <RepositoryItem repository={repository}/>
+                <RepositoryItem repository={repository}/>
+                <RepositoryItem repository={repository}/>
+                <RepositoryItem repository={repository}/>
                 <RepositoryItem repository={repository}/>
                 <RepositoryItem repository={repository}/>
                 <RepositoryItem repository={repository}/>
